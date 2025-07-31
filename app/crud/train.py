9135214e442eaa673eebf1b0c52dc73bcb064899
varada <varadavalsalya@gmail.com>
2025-07-31 13:20:11 +0530

    changes

--- app/crud/train.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.future import select
-from sqlalchemy.ext.asyncio import AsyncSession
-from app.models.train import Train
-
-
-async def get_train_names(db: AsyncSession):
-    """Get all train names and IDs"""
-    result = await db.execute(select(Train.id, Train.name))
-    return result.all()
